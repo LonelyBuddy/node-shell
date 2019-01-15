40be269e4301c448c6d5e7f843140198f6b1c18c
Abbey Mondshein <5883999+abbeymondshein@users.noreply.github.com>
2019-01-15 12:49:34 -0500

    switching

--- bash.js
+++ bash.js
@@ -2,11 +2,15 @@ process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
-
-  if (cmd === 'pwd') {
-    process.stdout.write(process.cwd());
-  } else {
-    process.stdout.write('You typed: ' + cmd);
+  const pwd = require('./pwd');
+  const ls = require('./ls')
+  const fs = require('fs')
+  if (cmd ===  'pwd'){
+    pwd()
+  } else if (cmd === 'ls'){
+    ls()
   }
+  // process.stdout.write('You typed: ' + pwd());
+  // process.stdout.write('You typed: ' + cmd);
   process.stdout.write('\nprompt > ');
 });
