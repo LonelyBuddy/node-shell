fdb98af7890f9bb84739c4dc80e539eef940b1ab
Henry Zheng <henryzheng92@gmail.com>
2019-01-15 13:03:16 -0500

    s

--- bash.js
+++ bash.js
@@ -5,10 +5,13 @@ process.stdin.on('data', (data) => {
   const pwd = require('./pwd');
   const ls = require('./ls')
   const fs = require('fs')
+  const cat = require('./cat');
   if (cmd ===  'pwd'){
     pwd()
   } else if (cmd === 'ls'){
     ls()
+  } else if (cmd === 'cat') {
+    cat(data);
   }
   // process.stdout.write('You typed: ' + pwd());
   // process.stdout.write('You typed: ' + cmd);
